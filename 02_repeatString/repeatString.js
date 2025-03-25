1b47fc72a1da78dfb9a58fdbd97b52516bd69479
Samuel Daligdig <daligdigsamuel812@gmail.com>
2025-03-25 12:35:38 +0800

    Completed 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(stringInput, loopTimes) {
+    let arrays = '';
+    for (let i = 0; i < loopTimes; i++) {
+        arrays += stringInput;
+    }
+   return arrays;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
