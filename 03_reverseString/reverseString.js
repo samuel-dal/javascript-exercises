d2c9bdd088f0359b78906021973076ca3b23efc0
Samuel Daligdig <daligdigsamuel812@gmail.com>
2025-03-25 12:52:48 +0800

    Completed 03_reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+    let name = inputString;
+    let reverseName = '';
 
+    for (let i = 1; i < name.length + 1; i++){
+        reverseName += name[name.length - i];
+    }
+    return reverseName;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
