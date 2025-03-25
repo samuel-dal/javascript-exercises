62c48b83ce48971e03f0459838de62954bde6ea2
Samuel Daligdig <daligdigsamuel812@gmail.com>
2025-03-25 13:43:58 +0800

    Completed 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(startNumber, endNumber) {
+    let finalSum = 0;
+    for (let i = startNumber; i < endNumber + 1; i++){
+        finalSum += i;
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
