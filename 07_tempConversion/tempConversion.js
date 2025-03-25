abcf4a65358b85d9b1f41e3b26ff64f2a294be32
Samuel Daligdig <daligdigsamuel812@gmail.com>
2025-03-25 14:22:01 +0800

    Completed 07_tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  return parseInt((((temperature - 32) * 5) / 9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  return parseInt((((temperature * 9) / 5) + 32).toFixed(1));
 };
+
+//convertToCelsius(32) // fahrenheit to celsius, should return 0
 
 // Do not edit below this line
 module.exports = {
