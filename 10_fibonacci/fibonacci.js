69840428fceb1d9ebe7d2c15d60fa6f4b43f8489
Samuel Daligdig <daligdigsamuel812@gmail.com>
2025-03-29 17:25:03 +0800

    Passed 10_fibonacci challenge

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(inputNum) {
+  let fibArr = [1, 1];
+  let lastFib;
+  for (let i = 0; i < inputNum - 2; i++) {
+    fibArr.push(parseInt(fibArr[fibArr.length - 1] + fibArr[fibArr.length - 2]));
+    lastFib = fibArr[fibArr.length - 1];
+  }
+  return lastFib;
 };
 
 // Do not edit below this line
