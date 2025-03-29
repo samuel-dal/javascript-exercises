023a636620310f2238c9558ac98168e7acb606ce
Samuel Daligdig <daligdigsamuel812@gmail.com>
2025-03-29 19:40:31 +0800

    Passed 12_findTheOldest challenge

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,34 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arrayPeople) {
+  let dateYearNow = (new Date().getFullYear());//2025
+  
+  let arrayOfAgeWithName = [];
+  arrayPeople.map((item, index) => {
+    arrayOfAgeWithName.push([item.name, parseInt(item.yearOfDeath - item.yearOfBirth), index]);
+  })
+  arrayOfAgeWithName.sort((a, b) => a[1] - b[1]);
+  arrayPeople
+  return arrayPeople[arrayOfAgeWithName[arrayOfAgeWithName.length - 1][2]];
 };
+
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+]
+
+findTheOldest(people).name;
 
 // Do not edit below this line
 module.exports = findTheOldest;
