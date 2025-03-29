f22a3b1b079b4811c2aba3fbc5843186e38677e8
Samuel Daligdig <daligdigsamuel812@gmail.com>
2025-03-29 16:02:44 +0800

    Passed 08_calculator challenge

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((acc, el) => acc + el, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(a, b) {
+  return a * b;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(factorialNum) {
+  let factorialResult = 1;
+	for (let i = factorialNum; i > 0; i--) {
+    factorialResult *= i;
+  }
+  return factorialResult;
 };
 
 // Do not edit below this line
