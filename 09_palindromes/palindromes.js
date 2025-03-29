8963e4c37fb742c4ee9b5cdeaf7d18259b1064c0
Samuel Daligdig <daligdigsamuel812@gmail.com>
2025-03-29 16:23:39 +0800

    Passed 09_palindromes challenge

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
-};
+const palindromes = function (inputVal) {
+  let reverseVal = '';
+  for (let i = inputVal.length - 1; i >= 0; i--) {
+    reverseVal += inputVal[i];
+  }
+  return reverseVal.toLowerCase() === inputVal.toLowerCase();
+};//for of
 
 // Do not edit below this line
 module.exports = palindromes;
